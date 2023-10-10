46ea8520a0f9cfc30ef8438cafde8783e4ca5d22
Bradley Kirks <spinnyorb23@aim.com>
2023-10-10 10:52:46 -0400

    loops through an arrray

---
+++ loops.js
@@ -0,0 +1,24 @@
+// say hello 3 times 
+for(let i = 0; i < 4 ;i++){
+    console.log(i)
+}
+
+
+const people =['todd', 'andrew', 'Derek']
+for(let i = 0; i < people.length; i++){
+    console.log('hello' + people[i])
+}
+
+
+
+
+function hello(){
+    console.log('hello')
+}
+
+
+for(i=0; i<5 ;i++){
+    console.log('B')
+}
+
+hello()
